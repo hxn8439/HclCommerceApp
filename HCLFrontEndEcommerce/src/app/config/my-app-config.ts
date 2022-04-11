1e1671139163a07e7652417ed9b484c869592769
Hamilton Nguyen <52980606+hxn8439@users.noreply.github.com>
2022-04-11 18:49:21 -0500

    okta login

---
+++ HCLFrontEndEcommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4msd17sDbYgKrb5d7',
+        issuer: 'https://dev-44353888.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
